892c296c2377fd841501573f99a32ab556202458
MAC <os@MACs-MacBook-Pro.local>
2024-06-03 21:33:31 +0700

    update

--- routes/payment.js
+++ routes/payment.js
@@ -17,15 +17,7 @@ router.get('/view-qr', function(req, res, next) {
   const username = req.session.username;
   const money = (req.session.money || 0) * 1000;
   const moneyFormatted = new Intl.NumberFormat('vi-VN').format(money);
-  const now = new Date();
-  now.setMinutes(now.getMinutes() + 15);
-  const expireTime = new Date(now);
-  const date = `${expireTime.getDate() < 10 ? '0' : ''}${expireTime.getDate()}`;
-  const month = `${expireTime.getMonth() + 1 < 10 ? '0' : ''}${expireTime.getMonth() + 1}`;
-  const minute = `${expireTime.getMinutes() < 10 ? '0' : ''}${expireTime.getMinutes()}`;
-  const hour = `${expireTime.getHours() < 10 ? '0' : ''}${expireTime.getHours()}`;
-  const expireStr = `${hour}:${minute} ${date}/${month}`;
-  res.render('view_qr', { username, money: moneyFormatted, expireStr });
+  res.render('view_qr', { username, money: moneyFormatted });
 });
 
 router.post('/update-payment', function(req, res) {
